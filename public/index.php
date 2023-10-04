8df3fb0048b613517d9fbb4c79cb0b5eeae507d2
Domantas <domantas.slaiciunas@dielektrik.com>
2023-10-04 20:45:38 +0300

    .

--- public/index.php
+++ public/index.php
@@ -4,9 +4,3 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-
-$invoice = new \App\Invoice();
-
-$invoice2 = new $invoice();
-
-var_dump($invoice, $invoice2, \App\Invoice::create());
