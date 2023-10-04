f1afa3b98427d75c1c0942f8153502b93191f6f9
Domantas <domantas.slaiciunas@dielektrik.com>
2023-10-04 21:19:21 +0300

    Serialize Objects & Serialize Magic Methods

---
+++ app/Invoice.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+class Invoice
+{
+    public string $id;
+
+    public function __construct(
+        public float  $amount,
+        public string $description,
+        public string $creditCardNumber
+    )
+    {
+        $this->id = uniqid('invoice_');
+    }
+
+    public function __serialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'amount' => $this->amount,
+            'description' => $this->description,
+            'creditCardNumber' => base64_encode($this->creditCardNumber),
+            'foo' => 'bar'
+        ];
+    }
+
+    public function __unserialize(array $data): void
+    {
+        $this->id = $data['id'];
+        $this->amount = $data['amount'];
+        $this->description = $data['description'];
+        $this->creditCardNumber = base64_decode($data['creditCardNumber']);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,5 +2,27 @@
 
 declare(strict_types=1);
 
+use App\Invoice;
+
 require __DIR__ . '/../vendor/autoload.php';
 
+$invoice = new Invoice(25, 'Invoice 1', '123456789');
+
+//echo serialize(true) . PHP_EOL;
+//echo serialize(1) . PHP_EOL;
+//echo serialize(2.5) . PHP_EOL;
+//echo serialize('Hello world') . PHP_EOL;
+//echo serialize([1, 2, 3]) . PHP_EOL;
+//var_dump(unserialize(serialize(['a' => 1, 'b' => 2]))) . PHP_EOL;
+
+//echo serialize($invoice) . PHP_EOL;
+
+//$str = serialize($invoice);
+//$invoice2 = unserialize($str);
+//var_dump($invoice, $invoice2, $invoice === $invoice2);
+
+//var_dump(unserialize('O:11:"App\Invoice":1:{s:2:"id";s:21:"invoice_651da74319e7f";}'));
+
+$str = serialize($invoice);
+echo $str . PHP_EOL;
+var_dump(unserialize($str));
